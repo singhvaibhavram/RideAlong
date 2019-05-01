d3679607a967d91e3c23e4090e12cec35d54ee84
singhvaibhavram <singh.vaibhav.ram@gmail.com>
2019-05-01 23:48:35 +0530

    Login  Register View

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RideAlong
-An App to Book Cabs
+An App to Book Cabs. This here is the Driver's App in making.
+
+**Cloud Storage:**
+Firebase has been used to store data for this project.
+
+**Libraries:**
+1. Firebase Authentication
+2. Firebase Database
+3. Support Design
+4. Calligraphy
+
+**Font:**
+1. Mooka
--- app/src/main/java/com/example/ridealong/MainActivity.java
+++ app/src/main/java/com/example/ridealong/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.ridealong;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Mooka_PersonalUse.otf")
+                .setFontAttrId(R.attr.fontPath).build());
         setContentView(R.layout.activity_main);
     }
 }
